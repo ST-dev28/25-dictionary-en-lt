231ed21a60220e10f7b7711ab0443ce05d20ec1b
SandraT <sndrbltr@gmail.com>
2021-07-07 17:26:33 +0000

    Edit words

--- js/components/Dictionary.js
+++ js/components/Dictionary.js
@@ -14,7 +14,8 @@ class Dictionary {
 
         // atnaujinu irasus
         this.updateFormDOM = null;
-        this.updateWordDOM = null;
+        this.updateEnWordDOM = null;
+        this.updateLtWordDOM = null;
         this.buttonUpdateDOM = null;
         this.buttonCancelDOM = null;
 
@@ -25,6 +26,8 @@ class Dictionary {
         this.localStorageMemoKey = 'memoList';
         this.latestUsedID = JSON.parse(localStorage.getItem(this.localStorageMemoIDcount)) || 0;
         this.savedWords = JSON.parse(localStorage.getItem(this.localStorageMemoKey)) || [];
+
+        this.wordBeingRevisedID = 0;
 
         this.init();
     }
@@ -37,6 +40,8 @@ class Dictionary {
             console.error('ERROR: Element not found!');
             return false
         }
+        this.DOM.classList.add('dic');
+
         this.render();
         this.addEvents();
         this.renderList();
@@ -60,15 +65,15 @@ class Dictionary {
                 <h2>Saved words<br>English - Lithuanian</h2>`
     }
     generateUpdateForm() {
-        return `<form id="update_word">
-                    <input id="upd_word" type="text" placeholder="type EN word" required>
-                    <input id="upd_word" type="text" placeholder="type LT word" required>
+        return `<form id="update_word" class="hide">
+                    <input id="upd_enWord" type="text" placeholder="type EN word" required>
+                    <input id="upd_ltWord" type="text" placeholder="type LT word" required>
                     <button id="update_button" type="submit">Update</button>
                     <button id="cancel_button" type="reset">Cancel</button>
                 </form>`;
     }
     generateList() {
-        return `<list class="words"></list>`
+        return `<div class="words"></div>`
     }
     renderList() {
         for (const word of this.savedWords) {
@@ -76,7 +81,7 @@ class Dictionary {
         };
     }
     renderDictionary(id, enWord, ltWord) {
-        const HTML = `<form class="memory">
+        const HTML = `<form id="memory_${id}"class="memory">
             <div class="memo" type="text">${enWord}</div>
             <div class="memo" type="text">${ltWord}</div>
             <button id="edit" type="button" href="html" class="fa fa-pencil" aria-hidden="true"></button>
@@ -104,16 +109,23 @@ class Dictionary {
         editDOM.addEventListener('click', () => {
             this.addFormDOM.classList.add('hide');
             this.updateFormDOM.classList.remove('hide');
-            console.log('editing...', enWord, ltWord);
+            //console.log('editing:', enWord, ltWord);
+
+            this.updateLtWordDOM.value = ltWord;
+            this.updateEnWordDOM.value = enWord;
+            this.wordBeingRevisedID = id;
+
+            //console.log('editing:', this.wordBeingRevisedID);
         })
     };
     render() {
         let HTML = ''
         HTML += this.generateAddForm();
+        HTML += this.generateUpdateForm();
         HTML += this.generateList();
         this.DOM.innerHTML = HTML;
 
-        this.listDOM = document.querySelector('list');
+        this.listDOM = document.querySelector('.words');
 
         this.addFormDOM = document.getElementById('add_word');
         this.enWordDOM = document.getElementById('en');
@@ -121,9 +133,11 @@ class Dictionary {
         this.buttonSaveDOM = document.getElementById('save');
 
         this.updateFormDOM = document.getElementById('update_word');
-        this.updateWordDOM = document.getElementById('upd_word');
+        this.updateLtWordDOM = document.getElementById('upd_ltWord');
+        this.updateEnWordDOM = document.getElementById('upd_enWord');
         this.buttonUpdateDOM = document.getElementById('update_button');
-        this.buttonCancelDOM = document.getElementById('update_cancel');
+        this.buttonCancelDOM = document.getElementById('cancel_button');
+        console.log(this.updateFormDOM);
     }
     addEvents() {
         // pridedu irasus
@@ -154,12 +168,35 @@ class Dictionary {
         // trinu irasa
 
         // grizti i pirmine forma
-        /*this.buttonCancelDOM.addEventListener('click', (e) => {
+        this.buttonCancelDOM.addEventListener('click', (e) => {
             e.preventDefault();
             this.addFormDOM.classList.remove('hide');
             this.updateFormDOM.classList.add('hide');
             // redaguoju irasa
-        })*/
+        })
+        this.buttonUpdateDOM.addEventListener('click', (e) => {
+            e.preventDefault();
+            const enWord = this.updateEnWordDOM.value;
+            const ltWord = this.updateLtWordDOM.value;
+            console.log(enWord, ltWord);
+
+            for (const word of this.savedWords) {
+                if (word.id === this.wordBeingRevisedID) {
+                    word.ltWord = lt;
+                    word.enWord = en;
+                }
+            }
+            localStorage.setItem(this.localStorageMemoKey, JSON.stringify(this.savedWords));
+
+            const memoryDOM = this.DOM.querySelector('#memory_' + this.wordBeingRevisedID);
+            console.log(this.wordBeingRevisedID);
+            const memoryWordDOM = memoryDOM.querySelectorAll('.memo');
+            memoryWordDOM[0].innerText = enWord;
+            memoryWordDOM[1].innerText = ltWord;
+            console.log(memoryWordDOM);
+            this.addFormDOM.classList.remove('hide');
+            this.updateFormDOM.classList.add('hide');
+        })
     };
 }
 
